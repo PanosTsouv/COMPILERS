0fd55d35cf71e07ec90fea4674f7b9a13de86316
PanosTsouv <panostsouv95@gmail.com>
2020-11-25 23:15:23 +0200

    add a production-test file

---
+++ Project/files/productionTest.py
@@ -0,0 +1,31 @@
+def comparisonTesting():
+	if not not true and true:
+		print 3
+	if not not not true:
+		print 3
+	if not true and true:
+		print 3
+	if not true and not not true:
+		print 3
+	if  not not not true and not not not true and not not not true:
+		print 3
+	if true and true and true:
+		print 3
+	if not x > 5 and y >= 4 and y <= 4 and x <= 10:
+		print 3
+def expressionTesting(a = 0,b = 0,c = 0):
+	if x <3+2:
+		print 3**8+2*4+2*3*4
+		print x[3]*get()**4+get(3,4)**8
+		print x[3]*get()**max(3,4,5)+get(3,4)**min(5,7,8)*[5,6,77]
+
+def generalTest(x, y=0, z, ar):
+	x = 0
+	z -= x**y
+	array[3+2] = 6+2
+	print x , y + z , ar , get(x, max(3,4,5))
+	while x >= 0 :
+		for x in array:
+			x /= y/z
+	if x <3+2:
+		return z
