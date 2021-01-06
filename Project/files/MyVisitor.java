416f17f5838ac03e897bd7c9f678b01c901adc43
PanosTsouv <panostsouv95@gmail.com>
2021-01-06 05:18:35 +0200

    Organize the code of MyVisitor - Store helpful data

--- Project/files/MyVisitor.java
+++ Project/files/MyVisitor.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.LinkedList;
 
@@ -7,71 +8,174 @@ import minipython.analysis.DepthFirstAdapter;
 
 public class MyVisitor extends DepthFirstAdapter
 {
-    private Hashtable symtable;	
+	private Hashtable symtable;
+	private String currentFuction = "";
+	Hashtable<String, Object> fData;
 
 	MyVisitor(Hashtable symtable) 
 	{
 		this.symtable = symtable;
-    }
-    
-    public void inADefFunction(ADefFunction node) 
+	}
+	
+    public void inADefFunction(ADefFunction node)
 	{
 		/*
-			This method check if a function with same name and same number of attributes is defined multiple times
-			If function is not defined -> add to Hashtable symtable
-			-ex -> x() is same with x(a = 0) , y(a, b) is same with y(a, b = 0)
+			This method create fData Hashtable which contains all info we need about current function
 
 			fname : String
 				The name of defined function
-			args : int
-				The number of all arguments(with default and non default values)
-			defaultArgs : int
-				The number of arguments with default values
-			line, pos : int
-				The position where function is defined
-			nextArgs : LinkedList with AAfterFirstArgNextArgs objects
-				Contains all next args after first arg
-			fKey : String
-				ex -> x(a, b) has fKey x(2) , x(a, b, c = 0) has fKey x(3)
-			fKeyWithoutDefaultArgs : String
-				ex -> x(a, b) has fKeyWithoutDefaultArgs x(2) , x(a, b, c = 0) has fKeyWithoutDefaultArgs x(2)
 
 			Used Methods:
 				getExpression() : PExpression object
 					PExpression object can be cast to all alternatives (AIdentifierExpression is PExpression)
 				getArgument() : LinkedList with AFirstArgument object (Exist in ADefFunction class)
 					Return the first argument of function - Could be empty or size 1
-				getNextArgs() : LinkedList with AAfterFirstArgNextArgs objects (Exist in AFirstArgument class)
-					Return all next args after first arg - Could be gteater or equal than 0
-				getValue() : LinkedList with default values (Exist in AFirstArgument and AAfterFirstArgNextArgs class)
-					Return the value of an arg - Could be empty or size 1
 		*/
 		String fName = ((AIdentifierExpression)node.getExpression()).getId().toString().trim();
+		fData = new Hashtable<>();
+		currentFuction = fName;
 		int args = node.getArgument().size();
-		int defaultArgs = 0;
-		if(args != 0)
-		{
-			defaultArgs = ((AFirstArgument)node.getArgument().get(0)).getValue().size();
-			LinkedList nextArgs = ((AFirstArgument)node.getArgument().get(0)).getNextArgs();
-			args = args + nextArgs.size();
-			for (Object iArg : nextArgs)
-			{
-				defaultArgs = defaultArgs + ((AAfterFirstArgNextArgs)iArg).getValue().size();
-			}
-		}
-		String fKey = fName + "(" + Integer.toString(args) + ")";
-		String fKeyWithoutDefaultArgs = fName + "(" + Integer.toString(args - defaultArgs) + ")";
+		fData.put("args", args);
+		fData.put("defaultArgs", 0);
+	}
 
-        int line = ((TId)((AIdentifierExpression)node.getExpression()).getId()).getLine();
+	public void outADefFunction(ADefFunction node)
+	{
+		/*
+			This method compare the args of current function with the args of fuction with the same name 
+			If a fuction with the same name exist and has same args - print an error message else add Data of this fuction
+
+			allFuctionWithSameName : String
+				All fuction with same name and different args have already visited
+			currentFunctionArgs : int
+				The number of args which check fuction contains
+			currentFunctionDefaultArgs : int
+				The number of default args which check fuction contains
+			line, pos : int
+				The position where function is defined
+			fuctionArgs : int
+				The number of args which fuction with same name has
+			fuctionDefaultArgs : int
+				The number of default args which fuction with same name has
+
+			Used Methods:
+				getExpression() : PExpression object
+					PExpression object can be cast to all alternatives (AIdentifierExpression is PExpression)
+		*/
+		ArrayList<Hashtable<String, Object>> allFuctionWithSameName;
+		int line = ((TId)((AIdentifierExpression)node.getExpression()).getId()).getLine();
 		int pos = ((TId)((AIdentifierExpression)node.getExpression()).getId()).getPos();
-
-		if (symtable.containsKey(fKey) || symtable.containsKey(fKeyWithoutDefaultArgs))
+		if(!symtable.containsKey(currentFuction))
 		{
-			System.out.println("[" + line + "," + pos + "]" + ": " +" Function " + fName +" is already defined");
+			allFuctionWithSameName = new ArrayList<>();
+			symtable.put(currentFuction, allFuctionWithSameName);
+			fData.put("name", currentFuction + allFuctionWithSameName.size());
+			allFuctionWithSameName.add(fData);
 		}
 		else
 		{
-			symtable.put(fKey, node);
+			allFuctionWithSameName = (ArrayList<Hashtable<String,Object>>)symtable.get(currentFuction);
+
+			for(Hashtable<String,Object> function : allFuctionWithSameName)
+			{
+				int currentFunctionArgs = (int)fData.get("args");
+				int currentFunctionDefaultArgs = (int)fData.get("defaultArgs");
+				int fuctionArgs = (int)function.get("args");
+				int fuctionDefaultArgs = (int)function.get("defaultArgs");
+				if((currentFunctionArgs >= (fuctionArgs - fuctionDefaultArgs) && currentFunctionArgs <= fuctionArgs)
+					|| (fuctionArgs >= (currentFunctionArgs - currentFunctionDefaultArgs) && fuctionArgs <= currentFunctionArgs))
+				{
+					print("[" + line + "," + pos + "]" + ": " +" Function " + currentFuction +" is already defined");
+					return;
+				}
+			}
+			fData.put("name", currentFuction + allFuctionWithSameName.size());
+			allFuctionWithSameName.add(fData);
 		}
+		currentFuction = "";
+	}
+
+	public void inAFirstArgument(AFirstArgument node)
+	{
+		/*
+			Check if first argument have default value - add +1 or 0 to fData(defaultAgs key)
+			Check the number of children(NextArgs) - add +number to fData(args key)
+
+			nextArgsSize : int
+				The number of all next arguments(with default and non default values)
+			defaultArgs : int
+				The number of first argument with default value(0 or 1)
+			nextArgs : LinkedList with AAfterFirstArgNextArgs objects
+				Contains all next args after first arg
+
+			Used Methods:
+				getNextArgs() : LinkedList with AAfterFirstArgNextArgs objects
+					Return all next args after first arg - Could be gteater or equal than 0
+				getValue() : LinkedList with default values
+					Return the value of an arg - Could be empty or size 1
+		*/
+		int defaultArgs = node.getValue().size();
+		defaultArgs = defaultArgs + (int)fData.get("defaultArgs");
+		fData.put("defaultArgs", defaultArgs);
+		LinkedList nextArgs = node.getNextArgs();
+		int nextArgsSize = (int)fData.get("args") + nextArgs.size();
+		fData.put("args", nextArgsSize);
+	}
+
+	public void inAAfterFirstArgNextArgs(AAfterFirstArgNextArgs node)
+	{
+		/*
+			Check if next argument have default value - add +1 or 0 to fData(defaultAgs key)
+
+			Variables:
+				defaultValueOfNextArgs : int
+					The number of next argument with default value(0 or 1)
+
+			Used Methods:
+				getValue() : LinkedList with default values
+					Return the value of an arg - Could be empty or size 1
+		*/
+		int defaultValueOfNextArgs = node.getValue().size();
+		defaultValueOfNextArgs = defaultValueOfNextArgs + (int)fData.get("defaultArgs");
+		fData.put("defaultArgs", defaultValueOfNextArgs);
+	}
+
+	public void inAFunctionCallExpression(AFunctionCallExpression node)
+	{
+		boolean findFunction = false;
+		LinkedList args = node.getArglistExps();
+		TId fCallTId = ((AIdentifierExpression)node.getIdExp()).getId();
+		String fCallname = fCallTId.toString().trim();
+		if(symtable.containsKey(fCallname))
+		{
+			for(Hashtable<String,Object> function : (ArrayList<Hashtable<String,Object>>)symtable.get(fCallname))
+			{
+				if(args.size() <= (int)function.get("args") && args.size() >= ((int)function.get("args") - (int)function.get("defaultArgs")))
+				{
+					Hashtable<String, Object> fCallData = new Hashtable<>();
+					fCallData.put("functionName", function.get("name"));
+					fCallData.put("returnType", "");
+					symtable.put(fCallname + "Call", fCallData);
+					findFunction = true;
+				}
+			}
+			if(!findFunction)
+			{
+				print("[" + fCallTId.getLine() + "," + fCallTId.getPos() + "]" + ": " +" Function " + fCallname +" is defined with different number of arguments");
+			}
+		}
+		else
+		{
+			if(currentFuction != "")
+			{
+				return;
+			}
+			print("[" + fCallTId.getLine() + "," + fCallTId.getPos() + "]" + ": " +" Function " + fCallname +" is not defined");
+		}
+	}
+
+	public void print(Object objectToPrint)
+	{
+		System.out.println(objectToPrint);
 	}
 }
