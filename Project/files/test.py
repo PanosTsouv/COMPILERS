2654493931d561219b63184c20ef91600b783558
PanosTsouv <panostsouv95@gmail.com>
2021-01-29 21:09:18 +0200

    Add new test examples

--- Project/files/test.py
+++ Project/files/test.py
@@ -1,4 +1,4 @@
-# -Comment error 1,2,3 to check errors from second visitor
+# -Comment error 1,2,3,4 to check errors from second visitor
 # -Second visitor doesn't work if first visitor finds errors
 
 # Error1 Wrong function cals (function is not defined)
@@ -9,9 +9,12 @@ def functionNotDefine1():
     # right
     return functionDefine()
 
+functionNotDefine1()
+
 def functionDefine():
     print(1)
 # right
+functionNotDefine1()
 functionDefine()
 
 # Error 2 function is already define
@@ -58,7 +61,17 @@ functionExist()
 functionExist(1,2,3)
 functionExist(1,2,3,4)
 
-# Error 4 use not define variables
+# Error 4 default arguments are followed by non default
+def args(a=0, b):
+    print(1)
+
+def args2(a, b=0, c):
+    print(1)
+
+def args3(a=0, b, c):
+    print(1)
+
+# Error 5 use not define variables
 
 def add(x,y):
     return x + y
@@ -72,7 +85,7 @@ print(w)
 w = 0
 v = a
 
-# Error 5 wrong use of a function(return type)
+# Error 6 wrong use of a function(return type)
 def voidFunction():
     print(1)
 
@@ -89,15 +102,53 @@ b = y("1","0") + 4
 
 c = voidFunction() + 1
 
-# Error 6 wrong call of a function(add different types)
+# Error 7 wrong call of a function(add different types)
 
 def add2(x,y):
     return x + y
 k="hello world"
 print(add2(2,k))
 
+def sub(x,y):
+    return x - y
+print(sub(2,k))
+
+def mult(x,y):
+    return x * y
+print(mult(None,k))
+
+def div(x,y):
+    return x / y
+print(div(2,k))
+
+def mod(x,y):
+    return x % y
+print(mod(2,k))
+
+def power(x,y):
+    return x ** y
+print(power(2,k))
+
+def add3(x,y):
+    return x + y
+r = type(k)
+print(add3(2,r))
+
 # this is right (string * number)
 def mult2(x,y):
     return x * y
 k="hello world"
 print(mult2(2,k))
+
+# Error 8 assign error
+k=type(k)
+k=2
+
+# Error 9 wrong operation with assign
+k -= type(k)
+k /= 2
+
+# Error 10 wrong passed args
+def argsPass(a="a", b=None):
+    print(1)
+argsPass(1,2)
